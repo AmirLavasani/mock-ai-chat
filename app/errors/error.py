4d8db5165903bf3540c0e2c3597ad134d3d0516a
Amir Lavasani <amirm.lavasani@gmail.com>
2023-11-16 16:59:14 +0330

    Add fetch error response

--- app/errors/error.py
+++ app/errors/error.py
@@ -1,12 +1,25 @@
 from typing import List
 from pydantic import BaseModel
-from fastapi import HTTPException, status
+from fastapi import HTTPException
+
 
 class InteractionCreateErrorResponse(BaseModel):
     type: str = "interaction.create_error"
     msg: str = "Interaction creation failed"
     loc: List[str] = ["controllers", "interaction"]
 
+
 HTTPInteractionCreateErrorResponse = HTTPException(
-    status_code=404, detail=InteractionCreateErrorResponse.model_dump()
+    status_code=404, detail=InteractionCreateErrorResponse().dict()
+)
+
+
+class InteractionFetchErrorResponse(BaseModel):
+    type: str = "interaction.fetch_error"
+    msg: str = "Interaction fetch failed"
+    loc: List[str] = ["controllers", "interaction"]
+
+
+HTTPInteractionFetchErrorResponse = HTTPException(
+    status_code=404, detail=InteractionFetchErrorResponse().dict()
 )
